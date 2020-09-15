da5a841a253f231d90dfe6b462591e49f8afc913
Andrew Pye <andrewp@metsi.co.uk>
2020-09-15 07:40:37 +0100

    New Build for Itch.IO

--- Assets/Scripts/DamageDealer.cs
+++ Assets/Scripts/DamageDealer.cs
@@ -14,15 +14,4 @@ public class DamageDealer : MonoBehaviour
         Destroy(gameObject);
     }
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
